d997003c83e8788f85ebc3dc99ae76542ca79a6d
adityac290887 <adityachiliveri290887@gmail.com>
2025-08-23 02:54:50 +0000

    changes done on 23rd Aug

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,6 @@ package com.mycompany.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World from AWS & i am learning maven & Jenkins & changes done to valid in jenkins, changes done in main !!!");
+        System.out.println("Hello World from AWS & i am learning maven & Jenkins & changes done to valid in jenkins, changes done in main, changes done on 23rd Aug !!!");
     }
 }
